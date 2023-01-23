2e7f8ca5ab2aff2c56d009325b661b9053fb101c
sgoral <Sebastian.Goral@md.de>
2023-01-23 10:31:30 +0100

    fix init script

--- init.sh
+++ init.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 original_git=$(which git)
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Intercept git commands
 function git() {
-  result=$(/Users/sebastiangoral/workspace/git_commands/venv/bin/python /Users/sebastiangoral/workspace/git_commands/git_override.py "$@")
+  result=$(python3 "$SCRIPT_DIR/git_override.py" "$@")
   if [ $? == 0 ]; then
     eval "$original_git" $result
   fi
