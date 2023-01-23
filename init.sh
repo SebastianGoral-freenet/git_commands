34b54293134a174466dbc17d4b285d7f8d69db08
sgoral <Sebastian.Goral@md.de>
2023-01-23 11:34:34 +0100

    add zsh support

--- init.sh
+++ init.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
+shell=$(ps -cp "$$" -o command="")
+if [ "$shell" = "zsh" ] || [ "$shell" = "-zsh" ]; then
+  SCRIPT_FILE="${(%):-%N}"
+  SCRIPT_DIR="${SCRIPT_FILE:a:h}"
+elif [ "$shell" = "bash" ] || [ "$shell" = "-bash" ]; then
+  SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+else
+  echo "git_commands: unsupported shell: $shell" >&2
+  return
+fi
+
 original_git=$(which git)
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Intercept git commands
 function git() {
   result=$(python3 "$SCRIPT_DIR/git_override.py" "$@")
-  if [ $? == 0 ]; then
+  if [ $? -eq 0 ]; then
     eval "$original_git" $result
   fi
 }
